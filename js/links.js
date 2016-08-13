2a3dce3c5c150e5f5e7d39990c37cb4e63f5854d
Eugene Levenetc <shumvlesu@gmail.com>
2016-08-14 01:04:51 +0200

    Fix url

--- js/links.js
+++ js/links.js
@@ -5,7 +5,7 @@ var links = [
 	{title: "Eugene Levenetc", url: null},
 	{title: "", url: null},
 	{title: "Github", url: "https://github.com/elevenetc"},
-	{title: "StackOverflow", url: "http://stackoverflow.com/users/798165/fox-in-socks?tab=profile"},
+	{title: "StackOverflow", url: "http://stackoverflow.com/users/798165/eleven?tab=profile"},
 	{title: "urledIn", url: "https://www.linkedin.com/in/elevenets"},
 	{title: "500px", url: "https://500px.com/e-levenetc"},
 	{title: "Twitter", url: "https://twitter.com/elevenetc"},
