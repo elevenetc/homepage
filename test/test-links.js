be9b41b6ca2a90f5ef24f62bde5e9d99904ca374
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-08-26 18:43:13 +0200

    Add links test

---
+++ test/test-links.js
@@ -0,0 +1,13 @@
+/**
+ * Created by eugene.levenetc on 26/08/16.
+ */
+const links = require('../js/links');
+const assert = require('assert');
+
+it('links should be valid', function () {
+	const regex = /^(ftp|http|https):\/\/[^ "]+$/;
+	for (let i = 0; i < links.length; i++) {
+		if (links[i].type === 'link')
+			assert.equal(regex.test(links[i].url), true, links[i].url);
+	}
+});
