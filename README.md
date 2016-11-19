a477f22b959846ab601b6dc977da05e15fcffd70
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-11-19 18:59:56 +0100

    Add node server and update readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# homepage
+# Homepage
 [![Build Status](https://travis-ci.org/elevenetc/homepage.svg?branch=master)](https://travis-ci.org/elevenetc/homepage)
+## Building static
+```
+webpack ./js/main.js ./js/out.js
+```
+## Running
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+const restify = require('restify');
+const fs = require('fs');
+
+const server = restify.createServer({
+    name: 'elevenetc.su',
+    version: '0.0.1',
+    log: {
+        trace: function (name, err) {
+            if (name) {
+                console.log(name);
+            } else if (err) {
+                console.log(err);
+            }
+        }
+    }
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/', restify.serveStatic({
+    directory: __dirname,
+    default: 'index.html'
+}));
+
+server.get('js/out.js', restify.serveStatic({
+    directory: __dirname,
+}));
+
+server.listen(3000, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
